c335a7efe25e47d02436614f589dd9c01b30a5fa
David Craven <david@craven.ch>
2019-01-03 13:19:00 +0100

    Add PointerEvent.

--- src/wayland/pointer.rs
+++ src/wayland/pointer.rs
@@ -2,7 +2,8 @@ use std::sync::Mutex;
 use wayland_client::{Proxy, NewProxy};
 pub use wayland_client::protocol::wl_pointer::WlPointer;
 pub use wayland_client::protocol::wl_pointer::RequestsTrait as PointerRequests;
-pub use wayland_client::protocol::wl_pointer::Event as PointerEvent;
+use wayland_client::protocol::wl_pointer::Event;
+pub use wayland_client::protocol::wl_pointer::ButtonState;
 use crate::wayland::event_queue::EventSource;
 use crate::wayland::surface::{SurfaceEvent, SurfaceUserData};
 
@@ -10,10 +11,10 @@ pub fn implement_pointer(pointer: NewProxy<WlPointer>) -> Proxy<WlPointer> {
     let mut event_source: Option<EventSource<SurfaceEvent>> = None;
     pointer.implement(move |event, _pointer| {
         match event.clone() {
-            PointerEvent::Enter {
+            Event::Enter {
                 surface,
-                surface_x: _,
-                surface_y: _,
+                surface_x: x,
+                surface_y: y,
                 serial: _,
             } => {
                 let user_data = surface
@@ -22,23 +23,54 @@ pub fn implement_pointer(pointer: NewProxy<WlPointer>) -> Proxy<WlPointer> {
                     .lock()
                     .unwrap();
                 event_source = Some(user_data.event_source.clone());
-                let event = SurfaceEvent::Pointer { event };
+                let event = SurfaceEvent::Pointer {
+                    event: PointerEvent::Enter { x, y }
+                };
                 event_source.as_ref().unwrap().push_event(event);
             },
-            //PointerEvent::Leave { surface, serial } => {},
-            //PointerEvent::Button { button, state, time, serial } => {},
-            //PointerEvent::Motion { surface_x, surface_y, time } => {},
+            Event::Leave { surface: _, serial: _ } => {
+                let event = SurfaceEvent::Pointer {
+                    event: PointerEvent::Leave
+                };
+                event_source.as_ref().unwrap().push_event(event);
+                event_source = None;
+            },
+            Event::Button { button, state, time, serial: _ } => {
+                let event = SurfaceEvent::Pointer {
+                    event: PointerEvent::Button {
+                        button,
+                        state,
+                        time,
+                    }
+                };
+                event_source.as_ref().unwrap().push_event(event);
+            },
+            Event::Motion { surface_x: x, surface_y: y, time } => {
+                let event = SurfaceEvent::Pointer {
+                    event: PointerEvent::Motion {
+                        x,
+                        y,
+                        time,
+                    }
+                };
+                event_source.as_ref().unwrap().push_event(event);
+            },
             //PointerEvent::Axis { axis, value, time } => {},
             //PointerEvent::AxisSource { axis_source } => {},
             //PointerEvent::AxisStop { axis, time } => {},
             //PointerEvent::AxisDiscrete { axis, discrete } => {},
             //PointerEvent::Frame {} => {},
-            _ => {
-                let event = SurfaceEvent::Pointer { event };
-                event_source.as_ref().unwrap().push_event(event);
-            },
+            _ => {},
         }
     }, ())
+}
+
+#[derive(Clone, Debug)]
+pub enum PointerEvent {
+    Enter { x: f64, y: f64 },
+    Leave,
+    Button { button: u32, state: ButtonState, time: u32 },
+    Motion { x: f64, y: f64, time: u32 },
 }
 
 // TODO handle cursor
